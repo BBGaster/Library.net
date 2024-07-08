317bc97ae592e4654cb61f9e03ad9e63773fbb94
Bruno Belviso <belbruno2004@gmail.com>
2024-07-08 09:39:40 +0200

    added razor

---
+++ WebApp/Controllers/BookController.cs
@@ -0,0 +1,119 @@
+﻿using Library.BLL.Model;
+using Library.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.PL.WebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var books = _bookService.GetAll();
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _bookService.Create(model);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(model);
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BookModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _bookService.Update(model);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(model);
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var book = _bookService.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _bookService.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View();
+        }
+    }
+}
